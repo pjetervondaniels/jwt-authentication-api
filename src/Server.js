89152aba414b5553c822a24d933f359a76d6bd7e
Pedro Daniel Jardim <pedro.d.jardim@hotmail.com>
2020-08-23 16:37:35 -0300

    verification user and DB connect

--- src/Server.js
+++ src/Server.js
@@ -8,10 +8,12 @@ const port = 3000;
 const DBconnect = 'mongodb+srv://usuarioC:30448298@aplicacoespessoais.jewxs.mongodb.net/AirCnC?retryWrites=true&w=majority';
 
 dotenv.config();
-
 mongoose.connect( DBconnect, { useNewUrlParser: true, useUnifiedTopology: true,}, 
     () => {console.log('Connected do db!')}
 )
+
+app.use(express.json());
+
 
 app.use('/api/user', authRoute);
 app.listen(port, () => console.log(`this server is online in port ${port}!`));
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,38 @@
+const User = require('../model/User');
+
 const router = require('express').Router();
 
-router.post('/register', (req, res) =>{
-    res.send('Register!');
+const Joi = require('joi');
+
+const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+})
+
+router.post('/register',  async (req, res) =>{
+
+    let user =  new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    try {
+        const value = await schema.validateAsync(user);
+    }
+    catch (err) {
+        console.log('FERROU!');
+        res.send(value);
+    }
+
+    try{
+        const savedUser =  await user.save();
+        res.send(savedUser);
+    }catch(err){
+        res.status(400).send(err);
+    }
+    
 })
 
 module.exports = router;
