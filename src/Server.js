56be828e1eb91628062db4c596968c84195ad689
Pedro Daniel Jardim <pedro.d.jardim@hotmail.com>
2020-08-23 10:47:48 -0300

    Auth route and server configuration

---
+++ src/Server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoute = require('./routes/Auth');
+const dotenv = require('dotenv');
+
+const app = express();
+const port = 3000;
+const DBconnect = 'mongodb+srv://usuarioC:30448298@aplicacoespessoais.jewxs.mongodb.net/AirCnC?retryWrites=true&w=majority';
+
+dotenv.config();
+
+mongoose.connect( DBconnect, { useNewUrlParser: true, useUnifiedTopology: true,}, 
+    () => {console.log('Connected do db!')}
+)
+
+app.use('/api/user', authRoute);
+app.listen(port, () => console.log(`this server is online in port ${port}!`));
