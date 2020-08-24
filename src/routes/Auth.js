4dcb7bc90a7a565d0f089f3aea883375e23fd4c9
Pedro Daniel Jardim <pedro.d.jardim@hotmail.com>
2020-08-24 10:32:07 -0300

    validation register and check email exists in database

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,29 +1,30 @@
 const User = require('../model/User');
-
 const router = require('express').Router();
-
 const Joi = require('joi');
+const {registerValidation} = require('../Validation');
 
-const schema = Joi.object({
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
-})
+
 
 router.post('/register',  async (req, res) =>{
+    // verificando erros no registro
+    const {error} = registerValidation(req.body);
 
+    if(error){
+        return res.send(error.details[0].message);
+    }
+
+    // verificando se o usuário já está cadastrado
+    const emailExist = await User.findOne({email: req.body.email});
+    if(emailExist){
+        return res.status(400).send('Email already exists!');
+    }
+
+    // cadastrando novo usuário
     let user =  new User({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
     });
-
-    try {
-        const value = await schema.validateAsync(user);
-    }
-    catch (err) {
-        res.send(value);
-    }
 
     try{
         const savedUser =  await user.save();
@@ -31,7 +32,7 @@ router.post('/register',  async (req, res) =>{
     }catch(err){
         res.status(400).send(err);
     }
-    
+
 })
 
 module.exports = router;
