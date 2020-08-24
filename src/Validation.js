87883d8fd17d765f829ecafc3d04c21bfaacbd93
Pedro Daniel Jardim <pedro.d.jardim@hotmail.com>
2020-08-24 11:15:50 -0300

    Login route

--- src/Validation.js
+++ src/Validation.js
@@ -14,7 +14,6 @@ const registerValidation = (data) => {
 const loginValidation = (data) => {
 
     const schema = Joi.object({
-        name: Joi.string().min(6).required(),
         email: Joi.string().min(6).required().email(),
         password: Joi.string().min(6).required()
     })
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -3,10 +3,9 @@ const Joi = require('joi');
 const bcrypt = require('bcryptjs');
 
 const User = require('../model/User');
-const {registerValidation} = require('../Validation');
+const {registerValidation, loginValidation} = require('../Validation');
 
-
-
+// rota de cadastro
 router.post('/register',  async (req, res) =>{
     // verificando erros no registro
     const {error} = registerValidation(req.body);
@@ -40,5 +39,26 @@ router.post('/register',  async (req, res) =>{
     }
 
 })
+
+// rota de login
+router.post('/login', async (req, res) => {
+    const {error} = loginValidation(req.body);
+
+    if(error){
+        return res.send(error.details[0].message);
+    }
+    // verificando se o email não existe no DB
+    const user = await User.findOne({email: req.body.email});
+    if(!user){
+        return res.status(400).send("Email or password is wrong!");
+    }
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if(!validPass){
+        return res.status(400).send("Email or password is wrong!");
+    }else{
+        res.send('Logged in!');
+    }
+})
+
 
 module.exports = router;
